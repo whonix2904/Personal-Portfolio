308f7be7e8f824381b9b84ea2d2034ba8dacc2ce
whonix2904 <sahilmachhi29@gmail.com>
2023-09-08 18:33:36 +0530

    not fixed yet

---
+++ main.js
@@ -0,0 +1,19 @@
+var tabs = document.getElementsByClassName('tab-link');
+var contents = document.getElementsByClassName('tab-content');
+
+function click(tabname) {
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove("active-link");
+    }
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].classList.remove("active-content");
+    }
+
+    var clickedTab = document.querySelector('[data-tab="' + tabname + '"]');
+    clickedTab.classList.add("active-link");
+
+    var tabContent = document.getElementById(tabname);
+    if (tabContent) {
+        tabContent.classList.add("active-content");
+    }
+}
