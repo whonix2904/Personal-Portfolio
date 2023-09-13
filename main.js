453efd9e88aa7cdf6d45172ed0a46ca5668d6aa5
whonix2904 <sahilmachhi29@gmail.com>
2023-09-13 14:42:00 +0530

    rf

--- main.js
+++ main.js
@@ -16,53 +16,14 @@ tabs.forEach((tab, index) => {
   });
 });
 
+// scroll reveal animations 
 
-
-function navReveal() {
-  var navAnimation = document.querySelectorAll("nav ul li");
-
-navAnimation.forEach((navA, index) => {
-  navA.style.animation =
-    "topIn 1.5s ease-out " + (index * 0.3 + 1.2) + "s forwards";
+var sr = ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  delay : 200,
+  duration: 1000,
+  interval:400,
 });
-}
 
-ScrollReveal().reveal('.page2', { afterReveal:navReveal()  });
-
-// var leftAnimation = document.querySelectorAll(".page1 .left h1,.page1 .left p");
-
-// leftAnimation.forEach((leftT, index) => {
-//   leftT.style.animation =
-//     "left-in 1.5s ease-out " + (index * 0.3 + 3.2) + "s forwards";
-// });
-
-// var rightAnimation = document.querySelectorAll(
-//   ".aboutMe .heading h1, .aboutMe .heading p, .aboutMe .heading div"
-// );
-// rightAnimation.forEach((right1, index) => {
-//   right1.style.animation =
-//     "right-in 1.5s ease-out " + (index * 0.3 + 3.2) + "s forwards";
-// });
-
-
-
-
-// var page = document.querySelector('.page');
-// var colors = Array.from(page.children).filter(element => element.tagName === 'DIV');
-// colors.forEach((color, index) => {
-//   color.style.animation = `scale-in-center 1.5s cubic-bezier(0.16, 0.94, 0.89, 1) ${index * 0.5}s forwards`;
-//   // console.log(colors);
-// });
-
-
-// var pages = document.querySelectorAll('.page1,.page2,.page3,.page4');
-
-// var observer = new IntersectionObserver(entries => {
-//   console.log(entries);
-// })
-
-// pages.forEach(page => {
-//   observer.observe(page)
-// });
-
-
+ScrollReveal().reveal('.navbar-li', sr);
